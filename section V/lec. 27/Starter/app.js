71cc8ef643e6689c17a9410507cdf182c1be7a9b
nmara <aga.czekala89@gmail.com>
2018-01-18 22:45:12 +0100

    test

--- section V/lec. 27/Starter/app.js
+++ section V/lec. 27/Starter/app.js
@@ -1,7 +1,25 @@
-var myApp = angular.module('myApp', []);
+var myApp = angular.module('myApp', ['ngRoute']); //[]--> here go services
 
-myApp.controller('mainController', ['$scope', function($scope) {
-    
+myApp.config(function($routeProvider) {
+    $routeProvider
+    .when('/', { //when I see this hash
+        templateUrl:'pages/main.html', //I'm gonna connect this page (physical location of my pages goes into '')
+        controller:'mainController' // to this controller
+    })
+    .when('/second', {
+        templateUrl: 'pages/second.html',
+        controller: 'secondController'
+    })
+});
 
-    
+myApp.controller('mainController', ['$scope', "$log", function($scope, $log) {
+
+    $scope.name = "main";
+
+}]);
+
+myApp.controller('secondController', ['$scope', "$log", function($scope, $log) {
+
+    $scope.name = "second";
+
 }]);
