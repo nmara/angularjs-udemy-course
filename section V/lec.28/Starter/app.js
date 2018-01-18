ba1ef42cdd4dfa03215a0fe04d9914a2e8300cad
nmara <aga.czekala89@gmail.com>
2018-01-18 23:09:14 +0100

    sec. V finished

--- section V/lec.28/Starter/app.js
+++ section V/lec.28/Starter/app.js
@@ -1,29 +1,34 @@
 var myApp = angular.module('myApp', ['ngRoute']);
 
 myApp.config(function ($routeProvider) {
-    
+
     $routeProvider
-    
+
     .when('/', {
         templateUrl: 'pages/main.html',
         controller: 'mainController'
     })
-    
+
     .when('/second', {
         templateUrl: 'pages/second.html',
         controller: 'secondController'
     })
-    
+
+    .when('/second/:num', { //":" - this calls a value
+        templateUrl: 'pages/second.html',
+        controller: 'secondController'
+    })
+
 });
 
 myApp.controller('mainController', ['$scope', '$log', function($scope, $log) {
-    
+
     $scope.name = 'Main';
-    
+
 }]);
 
-myApp.controller('secondController', ['$scope', '$log', function($scope, $log) {
-    
-    $scope.name = 'Second';
-    
-}]);
+myApp.controller('secondController', ['$scope', '$log', '$routeParams', function($scope, $log, $routeParams) {
+
+    $scope.num = $routeParams.num || 1;
+
+}]); // routeParams can give you a value to the url
