791264de1080f040cf8141ed62cc83dc4e35f30f
nmara <aga.czekala89@gmail.com>
2018-01-22 23:19:55 +0100

    adjusting form - btn to work with enter key

--- section VIII/lec. 42/Starter/app.js
+++ section VIII/lec. 42/Starter/app.js
@@ -5,61 +5,12 @@ var weatherApp = angular.module('weatherApp', ['ngRoute', 'ngResource']);
 
 //Here we connect pages to index.html - ROUTES
 
-weatherApp.config(function($routeProvider) {
-    $routeProvider
-    .when("/", {
-        templateUrl: "pages/home.html",
-        controller: 'homeController'
-    })
-    .when("/!/forecast", {
-        templateUrl: "pages/forecast.html",
-        controller: "forecastController"
-    })
-});
-
 //Custom service
 
-weatherApp.service('cityService', function() {
-    this.city = "New York";
-});
 
 //Controllers
 
-weatherApp.controller('homeController', ['$scope', 'cityService', function($scope, cityService){
-    $scope.city = cityService.city;
-     $scope.$watch('city', function() {
-        cityService.city = $scope.city;
-    })
-}]);
 
-weatherApp.controller('forecastController', ['$scope', '$resource', 'cityService', function($scope, $resource, cityService){
-    $scope.city = cityService.city;
-    
-    $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast?&appid=ceb1e19ab012fa84e9442205e74b43cc&", { callback: "JSON_CALLBACK" }, { get: {method: "JSONP" }});
-    
-    $scope.weatherResult = $scope.weatherAPI.get({ q:$scope.city });
-    
-    $scope.convertToCelsius = function(degK) {
-        return Math.round(degK - 273.15);
-    }
-    
-    $scope.convertToDate = function(dt) {
-        return new Date(dt * 1000); //to get right date
-    }
-}]);
 
 // Directives
 
-weatherApp.directive("weatherReport", function() {
-    return {
-        restrict: 'E',
-        templateUrl: 'directives/weatherReport.html',
-        replace: true,
-        scope: { //isolating scope
-            weatherTemp: "=", //because object
-            convertToStandard: "&", //because function
-            convertToDate: "&", //again - function
-            dateFormat: "@" //beacuse string     
-        }
-    }
-});
